4e737defe21dca207d4e812c19143546a670a573
sztxr <sthrpx.e@gmail.com>
2019-05-05 20:26:22 -0300

    update site

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,40 @@
 import '../sass/index.scss';
-
-console.log('JS Ready');
+import { log } from 'util';
 
 (function() {
-  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+  // Fix for window resize on mobile
   let originalHeight = window.innerHeight * 0.01;
-  // Then we set the value in the --vh custom property to the root of the document
   document.documentElement.style.setProperty('--vh', `${originalHeight}px`);
-
-  // We listen to the resize event
   window.addEventListener('resize touchmove', () => {
     let currentHeight = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${currentHeight}px`);
   });
+
+  // Glitch effect
+  const logo = document.querySelector('.logo');
+  const text = document.querySelector('h1');
+  const icons = document.querySelectorAll('i');
+
+  function glitchOnHover(e) {
+    if (e.type == 'mouseover') text.classList.add('glitched');
+    if (e.type == 'mouseleave') text.classList.remove('glitched');
+  }
+
+  ['mouseover', 'mouseleave'].forEach(e => {
+    logo.addEventListener(e, glitchOnHover);
+  })
+  icons.forEach(i => i.addEventListener('mouseover', glitchOnHover));
+  icons.forEach(i => i.addEventListener('mouseleave', glitchOnHover));
 })();
+
+// ASCII Console Art
+console.log(`%c
+
+       _/_/_/_/    _/_/_/  _/_/_/_/_/  _/_/_/_/_/  _/_/_/_/  _/_/_/
+      _/        _/              _/        _/      _/        _/    _/
+    _/_/_/      _/_/        _/          _/      _/_/_/    _/_/_/
+  _/              _/    _/            _/      _/        _/    _/
+_/_/_/_/  _/_/_/    _/_/_/_/_/      _/      _/_/_/_/  _/    _/
+
+
+Drop me a message! hello@eszter.dev`, "font-family:monospace");
